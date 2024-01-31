b4c133d36368497928c83f1ad68f932ae62f5bb5
Adithya K B <adithyakb93@gmail.com>
2024-01-31 19:13:39 +0530

    Change font to poppins , Change projectcard style to flex, Change in font weight in nav

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,66 +1,120 @@
-import Link from 'next/link';
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const Footer = () => {
-    return (
-        <div className="text-gray-600 body-font">
-            <hr className='mt-5'/>
-            <div className="mt-5 container px-2 py-15 mx-auto">
-                <div className="flex flex-wrap md:text-left text-center order-first">
-                <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-                    <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">Social Media</h2>
-                    <nav className="list-none mb-10">
-                    <li>
-                        <Link href={"#"} className="text-gray-600 hover:text-gray-800">Facebook</Link>
-                    </li>
-                    <li>
-                        <Link href={"https://www.instagram.com/_a_c_e_s._/"} className="text-gray-600 hover:text-gray-800">Instagram</Link>
-                    </li>
-                    <li>
-                        <Link href={"https://www.linkedin.com/in/aces2022/"} className="text-gray-600 hover:text-gray-800">LinkedIn</Link>
-                    </li>
-                    <li>
-                        <Link href={"#"} className="text-gray-600 hover:text-gray-800">Twitter</Link>
-                    </li>
-                    <li>
-                        <Link href={"https://github.com/aces2022"} className="text-gray-600 hover:text-gray-800">Github</Link>
-                    </li>
-                    </nav>
-                </div>
-                <div className="lg:w-2/4 md:w-1/2 w-full px-4">
-                    <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">Contributed By</h2>
-                    <nav className="list-none mb-10">
-                        <li>
-                            <Link href={"https://wwww.rahulab.vercel.app"} className="text-gray-600 hover:text-gray-800">1. Rahul A B</Link>
-                        </li>
-                        <li>
-                            <Link href={"#"} className="text-gray-600 hover:text-gray-800">2. Jose Antony</Link>
-                        </li>
-                        <li>
-                            <Link href={"https://www.vivekkj.in"} className="text-gray-600 hover:text-gray-800">3. Vivek K J</Link>
-                        </li>
-                        <li>
-                            <Link href={"https://www.joeljaison.me"} className="text-gray-600 hover:text-gray-800">4. Joel Jaison</Link>
-                        </li>
-                    </nav>
-                </div>
-                <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-                    <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">SUBSCRIBE</h2>
-                    <div className="flex xl:flex-nowrap md:flex-nowrap lg:flex-wrap flex-wrap justify-center items-end md:justify-start">
-                    <div className="relative w-40 sm:w-auto xl:mr-4 lg:mr-0 sm:mr-4 mr-2">
-                        <label htmlFor="footer-field" className="leading-7 text-sm text-gray-600">To Website Updates</label>
-                        <input type="text" id="footer-field" name="footer-field" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
-                    </div>
-                    <button className="lg:mt-2 xl:mt-0 flex-shrink-0 inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Button</button>
-                    </div>
-                    {/* <p className="text-gray-500 text-sm mt-2 md:text-left text-center">Bitters chicharrones fanny pack
+  return (
+    <div className="text-gray-600 body-font">
+      <hr className="mt-5" />
+      <div className="mt-5 container px-2 py-15 mx-auto">
+        <div className="flex flex-wrap md:text-left text-center order-first">
+          <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+            <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
+              Social Media
+            </h2>
+            <nav className="list-none mb-10">
+              <li>
+                <Link href={"#"} className="text-gray-600 hover:text-gray-800">
+                  Facebook
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href={"https://www.instagram.com/_a_c_e_s._/"}
+                  className="text-gray-600 hover:text-gray-800"
+                >
+                  Instagram
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href={"https://www.linkedin.com/in/aces2022/"}
+                  className="text-gray-600 hover:text-gray-800"
+                >
+                  LinkedIn
+                </Link>
+              </li>
+              <li>
+                <Link href={"#"} className="text-gray-600 hover:text-gray-800">
+                  Twitter
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href={"https://github.com/aces2022"}
+                  className="text-gray-600 hover:text-gray-800"
+                >
+                  Github
+                </Link>
+              </li>
+            </nav>
+          </div>
+          <div className="lg:w-2/4 md:w-1/2 w-full px-4">
+            <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
+              Contributed By
+            </h2>
+            <nav className="list-none mb-10">
+              <li>
+                <Link
+                  href={"https://wwww.rahulab.vercel.app"}
+                  className="text-gray-600 hover:text-gray-800"
+                >
+                  1. Rahul A B
+                </Link>
+              </li>
+              <li>
+                <Link href={"#"} className="text-gray-600 hover:text-gray-800">
+                  2. Jose Antony
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href={"https://www.vivekkj.in"}
+                  className="text-gray-600 hover:text-gray-800"
+                >
+                  3. Vivek K J
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href={"https://www.joeljaison.me"}
+                  className="text-gray-600 hover:text-gray-800"
+                >
+                  4. Joel Jaison
+                </Link>
+              </li>
+            </nav>
+          </div>
+          <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+            <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
+              SUBSCRIBE
+            </h2>
+            <div className="flex xl:flex-nowrap md:flex-nowrap lg:flex-wrap flex-wrap justify-center items-end md:justify-start">
+              <div className="relative w-40 sm:w-auto xl:mr-4 lg:mr-0 sm:mr-4 mr-2">
+                <label
+                  htmlFor="footer-field"
+                  className="leading-7 text-sm text-gray-600"
+                >
+                  To Website Updates
+                </label>
+                <input
+                  type="text"
+                  id="footer-field"
+                  name="footer-field"
+                  className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                />
+              </div>
+              <button className="lg:mt-2 xl:mt-0 flex-shrink-0 inline-flex text-white bg-indigo-500 border-0 py-2 px-5 focus:outline-none hover:bg-indigo-600 rounded">
+                Subscribe
+              </button>
+            </div>
+            {/* <p className="text-gray-500 text-sm mt-2 md:text-left text-center">Bitters chicharrones fanny pack
                         <br className="lg:block hidden"/>waistcoat green juice
                     </p> */}
-                </div>
-                </div>
-            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- components/navbar.jsx
+++ components/navbar.jsx
@@ -3,102 +3,203 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
-import { FaSun, FaMoon } from 'react-icons/fa';
-import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
+import { FaSun, FaMoon } from "react-icons/fa";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 const Navbar = ({ toggleDarkMode, darkMode }) => {
-    const [nav,setNav] = useState(false);
-    const handleNav = () => {
-        setNav(!nav);
-        if(!nav) {
-            document.body.style.overflow = 'hidden';
-        } else {
-            document.body.style.overflow = 'auto';
-        }
-    };
-    
-    return (
-        <nav className={`flex-between w-full mb-3 px-4 py-2 ${darkMode ? "dark-mode" : "light-mode"}`} style={{ zIndex: "12 !important", position: "fixed", top: 0, width: "100%"  }} >
-            <Link href='/' className="flex gap-4 flex-center">
-                {darkMode
-                    ? <Image src="/aces-white.png" alt="ACES Logo" width={90} height={70} />
-                    : <Image src="/aces.png" alt="ACES Logo" width={90} height={70} />
-                }
-                <p className="logo_text">ACES</p>
-            </Link>
-            <div className="justify-left">
-                <ul className="hidden sm:flex gap-4">
-                    <li className="logo_text hover:text-gray-600"> <Link href={"/"}> Home </Link> </li>
-                    <li className="logo_text hover:text-gray-600"> <Link href={"/event"}> Events </Link> </li>
-                    <li className="logo_text hover:text-gray-600"> <Link href={"/project"}> Project </Link> </li>
-                    <li className="logo_text hover:text-gray-600"> <Link href={"/team"}> Team </Link> </li>
-                    <li className="logo_text hover:text-gray-600"> <Link href={"/faculty"}> Faculty </Link> </li>
-                    <li className="logo_text hover:text-gray-600"> <Link href={"/studymaterial"}> Study Matrials </Link> </li>
-                    <li className="logo_text hover:text-gray-600">
-                        <button onClick={toggleDarkMode} >
-                            {darkMode
-                                ? <FaSun size={20} />
-                                : <FaMoon size={20} />
-                            }   
-                        </button>
-                    </li>
-                </ul>
-                
-            </div>            
+  const [nav, setNav] = useState(false);
+  const handleNav = () => {
+    setNav(!nav);
+    if (!nav) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+  };
 
-            {/* Mobile View */}
-            <div className="flex flex-center sm:hidden">
-                <button onClick={toggleDarkMode}>
-                    {darkMode
-                        ? <FaSun size={20} />
-                        : <FaMoon size={20} />
-                    }
-                </button>
-                <div onClick={handleNav} className="px-4 z-10 block  hover:text-gray-600">
-                    {nav
-                        ? <AiOutlineClose size={20} />
-                        : <AiOutlineMenu size={20} />
-                    }
-                </div>
-            </div>
-            {darkMode
-                ? <>
-                    <div className={
-                        nav
-                        ? 'sm:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300'
-                        : 'sm:hidden absolute top-0 left-[-100%] right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300'}>
-                        <div>
-                            <ul className="sm:flex font-satoshi font-semibold">
-                                <li className="p-4 text-4xl hover:text-gray-600" onClick={handleNav}> <Link href={"/"}> Home </Link> </li>
-                                <li className="p-4 text-4xl hover:text-gray-600" onClick={handleNav}> <Link href={"/event"}> Events </Link> </li>
-                                <li className="p-4 text-4xl hover:text-gray-600" onClick={handleNav}> <Link href={"/project"}> Project </Link> </li>
-                                <li className="p-4 text-4xl hover:text-gray-600" onClick={handleNav}> <Link href={"/team"}> Team </Link> </li>
-                                <li className="p-4 text-4xl hover:text-gray-600" onClick={handleNav}> <Link href={"/faculty"}> Faculty </Link> </li>
-                                <li className="p-4 text-4xl hover:text-gray-600" onClick={handleNav}> <Link href={"/studymaterial"}> Study Matrials </Link> </li>
-                            </ul>
-                        </div>  
-                    </div>
-                </> 
-                : <>
-                <div className={
-                    nav
-                    ? 'sm:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-white text-center ease-in duration-300'
-                    : 'sm:hidden absolute top-0 left-[-100%] right-0 bottom-0 flex justify-center items-center w-full h-screen bg-white text-center ease-in duration-300'}>
-                    <div>
-                        <ul className="sm:flex font-satoshi font-semibold">
-                            <li className="p-4 text-4xl hover:text-gray-600" onClick={handleNav}> <Link href={"/"}> Home </Link> </li>
-                            <li className="p-4 text-4xl hover:text-gray-600" onClick={handleNav}> <Link href={"/event"}> Events </Link> </li>
-                            <li className="p-4 text-4xl hover:text-gray-600" onClick={handleNav}> <Link href={"/project"}> Project </Link> </li>
-                            <li className="p-4 text-4xl hover:text-gray-600" onClick={handleNav}> <Link href={"/team"}> Team </Link> </li>
-                            <li className="p-4 text-4xl hover:text-gray-600" onClick={handleNav}> <Link href={"/faculty"}> Faculty </Link> </li>
-                            <li className="p-4 text-4xl hover:text-gray-600" onClick={handleNav}> <Link href={"/studymaterial"}> Study Matrials </Link> </li>
-                        </ul>
-                    </div>  
-                </div>
-            </>
+  return (
+    <nav
+      className={`flex-between w-full mb-3 px-4 py-2 ${
+        darkMode ? "dark-mode" : "light-mode"
+      }`}
+      style={{
+        zIndex: "12 !important",
+        position: "fixed",
+        top: 0,
+        width: "100%",
+      }}
+    >
+      <Link href="/" className="flex gap-4 flex-center">
+        {darkMode ? (
+          <Image src="/aces-white.png" alt="ACES Logo" width={90} height={70} />
+        ) : (
+          <Image src="/aces.png" alt="ACES Logo" width={90} height={70} />
+        )}
+        <p className="logo_text">ACES</p>
+      </Link>
+      <div className="justify-left">
+        <ul className="hidden sm:flex gap-4">
+          <li className="logo_text hover:text-gray-600">
+            {" "}
+            <Link href={"/"}> Home </Link>{" "}
+          </li>
+          <li className="logo_text hover:text-gray-600">
+            {" "}
+            <Link href={"/event"}> Events </Link>{" "}
+          </li>
+          <li className="logo_text hover:text-gray-600">
+            {" "}
+            <Link href={"/project"}> Project </Link>{" "}
+          </li>
+          <li className="logo_text hover:text-gray-600">
+            {" "}
+            <Link href={"/team"}> Team </Link>{" "}
+          </li>
+          <li className="logo_text hover:text-gray-600">
+            {" "}
+            <Link href={"/faculty"}> Faculty </Link>{" "}
+          </li>
+          <li className="logo_text hover:text-gray-600">
+            {" "}
+            <Link href={"/studymaterial"}> Study Materials </Link>{" "}
+          </li>
+          <li className="logo_text hover:text-gray-600">
+            <button onClick={toggleDarkMode}>
+              {darkMode ? <FaSun size={20} /> : <FaMoon size={20} />}
+            </button>
+          </li>
+        </ul>
+      </div>
+
+      {/* Mobile View */}
+      <div className="flex flex-center sm:hidden">
+        <button onClick={toggleDarkMode}>
+          {darkMode ? <FaSun size={20} /> : <FaMoon size={20} />}
+        </button>
+        <div
+          onClick={handleNav}
+          className="px-4 z-10 block  hover:text-gray-600"
+        >
+          {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+        </div>
+      </div>
+      {darkMode ? (
+        <>
+          <div
+            className={
+              nav
+                ? "sm:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300"
+                : "sm:hidden absolute top-0 left-[-100%] right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300"
             }
-        </nav>  
-    )
-}
+          >
+            <div>
+              <ul className="sm:flex font-satoshi font-semibold">
+                <li
+                  className="p-4 text-4xl hover:text-gray-600"
+                  onClick={handleNav}
+                >
+                  {" "}
+                  <Link href={"/"}> Home </Link>{" "}
+                </li>
+                <li
+                  className="p-4 text-4xl hover:text-gray-600"
+                  onClick={handleNav}
+                >
+                  {" "}
+                  <Link href={"/event"}> Events </Link>{" "}
+                </li>
+                <li
+                  className="p-4 text-4xl hover:text-gray-600"
+                  onClick={handleNav}
+                >
+                  {" "}
+                  <Link href={"/project"}> Project </Link>{" "}
+                </li>
+                <li
+                  className="p-4 text-4xl hover:text-gray-600"
+                  onClick={handleNav}
+                >
+                  {" "}
+                  <Link href={"/team"}> Team </Link>{" "}
+                </li>
+                <li
+                  className="p-4 text-4xl hover:text-gray-600"
+                  onClick={handleNav}
+                >
+                  {" "}
+                  <Link href={"/faculty"}> Faculty </Link>{" "}
+                </li>
+                <li
+                  className="p-4 text-4xl hover:text-gray-600"
+                  onClick={handleNav}
+                >
+                  {" "}
+                  <Link href={"/studymaterial"}> Study Materials </Link>{" "}
+                </li>
+              </ul>
+            </div>
+          </div>
+        </>
+      ) : (
+        <>
+          <div
+            className={
+              nav
+                ? "sm:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-white text-center ease-in duration-300"
+                : "sm:hidden absolute top-0 left-[-100%] right-0 bottom-0 flex justify-center items-center w-full h-screen bg-white text-center ease-in duration-300"
+            }
+          >
+            <div>
+              <ul className="sm:flex font-satoshi font-semibold">
+                <li
+                  className="p-4 text-4xl hover:text-gray-600"
+                  onClick={handleNav}
+                >
+                  {" "}
+                  <Link href={"/"}> Home </Link>{" "}
+                </li>
+                <li
+                  className="p-4 text-4xl hover:text-gray-600"
+                  onClick={handleNav}
+                >
+                  {" "}
+                  <Link href={"/event"}> Events </Link>{" "}
+                </li>
+                <li
+                  className="p-4 text-4xl hover:text-gray-600"
+                  onClick={handleNav}
+                >
+                  {" "}
+                  <Link href={"/project"}> Project </Link>{" "}
+                </li>
+                <li
+                  className="p-4 text-4xl hover:text-gray-600"
+                  onClick={handleNav}
+                >
+                  {" "}
+                  <Link href={"/team"}> Team </Link>{" "}
+                </li>
+                <li
+                  className="p-4 text-4xl hover:text-gray-600"
+                  onClick={handleNav}
+                >
+                  {" "}
+                  <Link href={"/faculty"}> Faculty </Link>{" "}
+                </li>
+                <li
+                  className="p-4 text-4xl hover:text-gray-600"
+                  onClick={handleNav}
+                >
+                  {" "}
+                  <Link href={"/studymaterial"}> Study Materials </Link>{" "}
+                </li>
+              </ul>
+            </div>
+          </div>
+        </>
+      )}
+    </nav>
+  );
+};
 
 export default Navbar;
